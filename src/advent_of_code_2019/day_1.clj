7ba43e3d1407a3b22e661d654fcbf08a48c54442
jasminzukic <jasminzukic2@gmail.com>
2019-12-01 10:02:56 +0100

    day1 finished and refactored

--- src/advent_of_code_2019/day_1.clj
+++ src/advent_of_code_2019/day_1.clj
@@ -1,10 +1,11 @@
 (ns advent-of-code-2019.day-1
-  (:require [advent-of-code-2019.common.core :refer [words]]
-            [advent-of-code-2019.common.core :as common]
-            [clojure.java.io :as io]))
+  (:require [clojure.java.io :as io]
+            [clojure.string :as str]))
 
-(defn part-1 []
-  (let [s (slurp (io/resource "inputs/day-1-input.txt"))]
-    (first (common/lines s))))
-
-(part-1)
+(defn day-1 []
+  (let [xs (->> "inputs/day-1-input.txt" io/resource slurp str/split-lines (map #(Integer/parseInt %)))
+        fuel (fn [x] (- (quot x 3) 2))
+        fuels (fn [x] (->> x fuel (iterate fuel) (take-while #(<= 0 %))))
+        day1a (apply + (map fuel xs))
+        day1b (apply + (mapcat fuels xs))]
+    [day1a day1b]))
